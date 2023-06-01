0ac5d90e24660a4171a14317a978a17587f2aa90
avokeys <alexander.ayala.palacin@gmail.com>
2023-06-01 00:48:55 +0000

    console logging all possible outcomes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let outcomes = [];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  pronoun.forEach(pro => {
+    adj.forEach(a => {
+      noun.forEach(n => {
+        let domain = `${pro}${a}${n}.com`;
+        outcomes.push(domain);
+      });
+    });
+  });
+  console.log(outcomes, "outcomes");
+  outcomes.forEach(outcome => {
+    document.querySelectorAll("#domain").innerHTML = `<li>${outcome}</li>`;
+  });
 };
